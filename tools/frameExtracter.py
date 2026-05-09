51ef5fca315f8c9b24888ad7cd36a6f5c3336ea5
Luc Le Gaillard <luc.legaillard@unicaen.fr>
2026-05-09 21:19:41 +0200

    Fix frameExtracter arg parsing and frame saving
    Tool works with frame extracted every step between start and end. Default step is 500ms.
    This reduces number of images extracted from video file.

--- tools/frameExtracter.py
+++ tools/frameExtracter.py
@@ -2,16 +2,25 @@ import os, sys
 import cv2, decord, numpy as np
 
 # Command line arguments
+# The path to the video file
 path = sys.argv[1]
-"""The path to the video file"""
+# The output directory for the extracted frames
 output = sys.argv[2]
-"""The output directory for the extracted frames"""
-startSeconds = float(sys.argv[3]) or 0
-"""The starting timestamp in seconds. It allows skipping the opening credits and going directly to the main content."""
-endSeconds = float(sys.argv[4]) or None
-"""The ending timestamp in seconds. It allows trimming video's ending credits."""
-step = float(sys.argv[5]) or 0.5
-"""The time interval in seconds between extracted frames"""
+# The starting timestamp in seconds. It allows skipping the opening credits and going directly to the main content.
+try:
+    startSeconds = float(sys.argv[3])
+except IndexError:
+    startSeconds = 0
+# The ending timestamp in seconds. It allows trimming video's ending credits.
+try:
+    endSeconds = float(sys.argv[4])
+except IndexError:
+    endSeconds = None
+# The time interval in seconds between extracted frames
+try:
+    step = float(sys.argv[5])
+except IndexError:
+    step = 0.5
 
 def getFrame(timestamp: float) -> np.ndarray:
     """Get the frame at the specified timestamp in seconds."""
@@ -21,22 +30,20 @@ def getFrame(timestamp: float) -> np.ndarray:
 # Load the video using decord
 vr = decord.VideoReader(path)
 fps = vr.get_avg_fps()
-# Calculate frame indices for both start and end points
-startFrameIndex = round(startSeconds * fps)
-# Get the timestamps for all frames in the video
-# The function returns two values for each frame: start and end timestamps for that frame. We take the mean to get a single timestamp for each frame.
-timestamps = vr.get_frame_timestamp(range(len(vr))).mean(-1)
 
-
-endSeconds = endSeconds or len(vr) / fps
+# Cap the end time to the video's duration
+maxSeconds = len(vr) / fps
+if endSeconds:
+    endSeconds = min(endSeconds, maxSeconds)
+else:
+    endSeconds = maxSeconds
 
 # Save each frame in the specified range as a JPEG image, named by its timestamp in milliseconds
-for i in range(startSeconds, endSeconds, step):
-    frame = getFrame(i)
-    timestamp = i
-    filename = f'{output}/{stamp:.0f}.jpg'
+for timestamp in range(int(startSeconds * 1000), int(endSeconds * 1000), int(step * 1000)):
+    frame = getFrame(timestamp / 1000)
+    filename = f'{output}/{timestamp:.0f}.jpg'
     if os.path.exists(filename):
         print(f'{filename} already exists, skipping')
         continue
     # Save the frame
-    cv2.imwrite(f'{output}/{stamp:.0f}.jpg', cv2.cvtColor(frame.asnumpy(), cv2.COLOR_RGB2BGR))
+    cv2.imwrite(filename, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
